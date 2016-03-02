99fa3f7d703ca81b094afad13e4f3bff93ec2cdb
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-03-02 10:36:08 -0600

    Add all circles to Test A

--- www/js/factories/circle.factory.js
+++ www/js/factories/circle.factory.js
@@ -2,11 +2,14 @@ angular.module('starter.factories').
   factory('Circle', function() {
     var Circle = function (){
       return [
-        { number: 1, screenX: '10px', screenY: '10px' },
-        { number: 2, screenX: '50px', screenY: '50px' },
-        { number: 3, screenX: '90px', screenY: '90px' },
-        { number: 4, screenX: '130px', screenY: '130px' },
-        { number: 5, screenX: '170px', screenY: '170px' }
+        { number: 1, screenX: '50%', screenY: '50%', label: 'Begin' },
+        { number: 2, screenX: '70%', screenY: '20%', label: '' },
+        { number: 3, screenX: '90%', screenY: '50%', label: '' },
+        { number: 4, screenX: '70%', screenY: '40%', label: '' },
+        { number: 5, screenX: '80%', screenY: '80%', label: '' },
+        { number: 6, screenX: '15%', screenY: '82%', label: '' },
+        { number: 7, screenX: '10%', screenY: '40%', label: '' },
+        { number: 8, screenX: '40%', screenY: '35%', label: 'End' }
       ];
     };
 
