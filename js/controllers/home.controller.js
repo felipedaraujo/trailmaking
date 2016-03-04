40d7a558edef18433c22baef1532156f54724160
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-03-04 11:09:14 -0600

    Initiate draw line feature

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -1,6 +1,17 @@
 angular.module('starter.controllers').
-  controller('HomeCtrl', function($scope, Circle) {
+  controller('HomeCtrl', function($scope, $document, $interval, $compile, Circle) {
+
+    var content = document.getElementsByClassName('scroll-content')[0];
+
     $scope.init = function() {
-      $scope.circles = new Circle();
+      $scope.circles = Circle.testA();
+    };
+
+    $scope.drawLine = function(event) {
+      var center = event.gesture.center;
+      var pixel = '<div style="position:absolute;left:'+center.pageX+'px;'+
+                  'top:'+center.pageY+'px;width:10px;height:10px;'+
+                  'border-radius:50%;background-color:#dadada"></div>'
+      content.innerHTML += pixel;
     };
   });
