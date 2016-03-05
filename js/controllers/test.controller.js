f1fe8dfb72661acfb481f031e2727feafcceee6e
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-03-05 15:42:27 -0600

    Refact draw line method

--- js/controllers/test.controller.js
+++ js/controllers/test.controller.js
@@ -1,79 +1,102 @@
 angular.module('starter.controllers').
   controller('TestCtrl', function($interval, $ionicGesture, $scope, $stateParams, Circle, Timer) {
 
-    var endCircleCenter;
-
-    var part;
-    var timer;
-    var time = 0;
-
     var Ax, Ay, Bx, By;
-
-    $scope.canvas.container = angular.element(document.querySelector('#canvas'));
-    $scope.canvas.context = $scope.canvas.container[0].getContext("2d");
+    var skipLoop = false;
+    var timer, time = 0;
+    var lastCircleCenter;
+    var canvasInitiate = false;
 
     $scope.init = function() {
-      part = $stateParams.part;
-      $scope.circles = new Circle(part);
-
-      if (part == 'partA') Timer.start();
-
-      setEndCircleCenter();
-      setCanvasSize();
+      $scope.circles = new Circle($stateParams.part);
     };
 
-    $ionicGesture.on('drag', function(event) {
-      if ($scope.canvas.begin) {
-        Ax = Ay = null;
-        $scope.canvas.context.beginPath();
-        $scope.canvas.begin = false;
-      }
+    $scope.drawLine = function(event) {
+      if (skipLoop) { skipLoop = false; return; }
+      skipLoop = true;
+
+      if (!canvasInitiate) canvasInit();
 
       var center = event.gesture.center;
-      var isEnd = isEndCircle(center);
+      var isLast = isLastCircle(center);
 
-      if (part == 'partA' && isEnd) alert(Timer.stop());
+      if ($stateParams.part == 'partA' && isLast) alert(Timer.stop());
+
+      if ($scope.canvas.reset) resetLine();
 
       Bx = center.pageX,
       By = center.pageY,
       Ax = Ax || Bx;
       Ay = Ay || By;
 
+      console.log($scope.canvas);
+
+      $scope.canvas.context.lineCap = "round";
+      $scope.canvas.context.beginPath();
       $scope.canvas.context.moveTo(Ax, Ay);
       $scope.canvas.context.lineTo(Bx, By);
+      $scope.canvas.context.lineWidth = 8;
+      $scope.canvas.context.strokeStyle = '#757575';
       $scope.canvas.context.stroke();
 
       Ax = Bx;
       Ay = By;
-
-    }, $scope.canvas.container);
-
-    content = function(){
-      return document.getElementsByClassName('scroll-content')[0];
     }
 
-    circleCenter = function(circle) {
+    canvasInit = function() {
+      $scope.canvas.container = angular.element(document.querySelector('#canvas'));
+      $scope.canvas.context = $scope.canvas.container[0].getContext("2d");
+
+      var content = getContentSize();
+      var lastCircle = $scope.circles[$scope.circles.length - 1];
+
+      setCanvasSize(content);
+      lastCircleCenter = getCircleCenter(content, lastCircle);
+
+      if ($stateParams.part == 'partA') Timer.start();
+      canvasInitiate = true;
+    }
+
+    resetLine = function(){
+      Ax = Ay = null;
+      $scope.canvas.reset = false;
+    };
+
+    isLastCircle = function(touch) {
+      var center = lastCircleCenter;
+      return (
+        (touch.pageX >= center.x - 25 && touch.pageX <= center.x + 25) &&
+        (touch.pageY >= center.y - 25 && touch.pageY <= center.y + 25)
+      );
+    }
+
+    beginPath = function() {
+      Ax = Ay = null;
+      $scope.canvas.context.beginPath();
+      $scope.canvas.begin = false;
+    }
+
+    getContentSize = function() {
+      var div = angular.element(document.querySelector('.scroll-content'));
+      return {
+        width: div[0].offsetWidth,
+        height: div[0].offsetHeight
+      };
+    }
+
+    setCanvasSize = function(content) {
+      $scope.canvas.container[0].width = content.width;
+      $scope.canvas.container[0].height = content.height;
+    }
+
+    getCircleCenter = function(content, circle) {
       var screenX = circle.screenX.replace('%','');
       var screenY = circle.screenY.replace('%','');
 
       return {
-        xAxis: content().offsetWidth * screenX / 100,
-        yAxis: content().offsetHeight * screenY / 100
+        x: content.width * screenX / 100,
+        y: content.height * screenY / 100
       }
-    }
-
-    isEndCircle = function(position) {
-      var A = endCircleCenter, B = position;
-      return (B.pageX >= A.xAxis - 25 && B.pageX <= A.xAxis + 25) && (B.pageY >= A.yAxis - 25 && B.pageY <= A.yAxis + 25);
-    }
-
-    setEndCircleCenter = function() {
-      endCircleCenter  = circleCenter($scope.circles[$scope.circles.length - 1]);
-    }
-
-    setCanvasSize = function() {
-      $scope.canvas.container[0].width = content().offsetWidth;
-      $scope.canvas.container[0].height = content().offsetHeight;
     }
 
   });
