71008c2419172f1920c08e0a1499308ee950cbf3
Felipe de Araujo <oifelipearaujo@gmail.com>
2016-03-11 15:24:00 -0600

    Add applicants list to home

--- www/js/controllers/home.controller.js
+++ www/js/controllers/home.controller.js
@@ -1,7 +1,19 @@
 angular.module('starter.controllers').
   controller('HomeCtrl', function($scope, $location) {
 
+    $scope.init = function() {
+      $scope.applicants = getAll();
+    }
+
     $scope.goTo = function(path) {
       $location.path(path);
+    }
+
+    getAll = function() {
+      var all = [];
+      for (var i = window.localStorage.length - 1; i >= 0; i--) {
+        all.push(JSON.parse(window.localStorage[i]));
+      }
+      return all;
     }
   });
